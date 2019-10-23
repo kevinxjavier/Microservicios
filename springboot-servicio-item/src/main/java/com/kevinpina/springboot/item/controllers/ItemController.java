926e7c5e7099ee3bfa96eb9c3597ed72f0bc596b
Kevin Piña <kevin@kevinpina.com>
2019-10-23 15:20:17 -0300

    Actualizando comentarios

--- springboot-servicio-item/src/main/java/com/kevinpina/springboot/item/controllers/ItemController.java
+++ springboot-servicio-item/src/main/java/com/kevinpina/springboot/item/controllers/ItemController.java
@@ -8,6 +8,7 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -27,13 +28,16 @@ public class ItemController {
 //	@Qualifier("itemServiceImpl")
 	private ItemService itemService;
 	
+	@Autowired
+	private Environment env;
+	
+	@Value("${configuracion.texto}")
+	private String texto;
+	
 	@GetMapping("/listar")
 	public List<Item> listar() {
 		return itemService.findAll();
 	}
-	
-	@Value("${configuracion.texto}")
-	private String texto;
 	
 	@HystrixCommand(fallbackMethod = "metodoAlternativo")
 	@GetMapping("/ver/{id}/cantidad/{cantidad}")
@@ -69,6 +73,12 @@ public class ItemController {
 		Map<String, String> json = new HashMap<>();
 		json.put("texto", texto);
 		json.put("port", port);
+		
+		if (env.getActiveProfiles().length > 0 && env.getActiveProfiles()[0].equals("dev")) { // el indice [0] es el nombre del profile activo 
+			json.put("autor.nombre", env.getProperty("configuracion.autor.nombre"));
+			json.put("autor.email", env.getProperty("configuracion.autor.email"));
+		}
+		
 		return new ResponseEntity<Map<String, String>>(json, HttpStatus.OK);
 	}
 }
