b52425b2409254d4493afb7ded62205bdfd2661e
Kevin Piña <kevin@kevinpina.com>
2019-10-25 01:13:21 -0300

    Agregando algunos comentarios

--- springboot-servicio-item/src/main/java/com/kevinpina/springboot/item/SpringbootServicioItemApplication.java
+++ springboot-servicio-item/src/main/java/com/kevinpina/springboot/item/SpringbootServicioItemApplication.java
@@ -1,7 +1,9 @@
 package com.kevinpina.springboot.item;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
@@ -12,6 +14,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableCircuitBreaker	// Habilitando Hystrix, para manejar posibles errores
 @EnableFeignClients
 @SpringBootApplication
+// @EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})	// Aca deshabilitamos la autoconfiguracion del DataSource si nos diera problemas la url del DataSource.  
 public class SpringbootServicioItemApplication {
 
 	public static void main(String[] args) {
